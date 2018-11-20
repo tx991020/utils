917716b4a86acf37fae8596f8b3410197bc4f455
吴迪 <wudi@hetao101.com>
2018-11-20 22:07:08 +0800

    add bcrypt

--- encode.go
+++ encode.go
@@ -1,1 +1,12 @@
 package utils
+
+import "golang.org/x/crypto/bcrypt"
+
+func CryptPass(pass string) string {
+	bytes, _ := bcrypt.GenerateFromPassword([]byte(pass), bcrypt.DefaultCost)
+	return string(bytes)
+}
+
+func ComparePassword(hash, password string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+}
