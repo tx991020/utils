a24ab096c3d17bf9220809dc438266f9e7dbca2b
吴迪 <wudi@hetao101.com>
2018-12-14 17:26:37 +0800

    add map2struct

--- json.go
+++ json.go
@@ -77,14 +77,19 @@ func SetField(obj interface{}, name string, value interface{}) error {
 
 	structFieldType := structFieldValue.Type()
 	val := reflect.ValueOf(value)
+	var err error
 	if structFieldType != val.Type() {
-		return errors.New("Provided value type didn't match obj field type")
+		val, err = TypeConversion(fmt.Sprintf("%v", value), structFieldValue.Type().Name()) //类型转换
+		if err != nil {
+			return err
+		}
 	}
 
 	structFieldValue.Set(val)
 	return nil
 }
 
+//map 转结构体
 func FillStruct(m map[string]interface{}, o interface{}) error {
 	for k, v := range m {
 		err := SetField(o, k, v)
@@ -95,6 +100,42 @@ func FillStruct(m map[string]interface{}, o interface{}) error {
 	return nil
 }
 
+
+
+//类型转换
+func TypeConversion(value string, ntype string) (reflect.Value, error) {
+	if ntype == "string" {
+		return reflect.ValueOf(value), nil
+	} else if ntype == "time.Time" {
+		t, err := time.ParseInLocation("2006-01-02 15:04:05", value, time.Local)
+		return reflect.ValueOf(t), err
+	} else if ntype == "Time" {
+		t, err := time.ParseInLocation("2006-01-02 15:04:05", value, time.Local)
+		return reflect.ValueOf(t), err
+	} else if ntype == "int" {
+		i, err := strconv.Atoi(value)
+		return reflect.ValueOf(i), err
+	} else if ntype == "int8" {
+		i, err := strconv.ParseInt(value, 10, 64)
+		return reflect.ValueOf(int8(i)), err
+	} else if ntype == "int32" {
+		i, err := strconv.ParseInt(value, 10, 64)
+		return reflect.ValueOf(int64(i)), err
+	} else if ntype == "int64" {
+		i, err := strconv.ParseInt(value, 10, 64)
+		return reflect.ValueOf(i), err
+	} else if ntype == "float32" {
+		i, err := strconv.ParseFloat(value, 64)
+		return reflect.ValueOf(float32(i)), err
+	} else if ntype == "float64" {
+		i, err := strconv.ParseFloat(value, 64)
+		return reflect.ValueOf(i), err
+	}
+
+	//else if .......增加其他一些类型的转换
+
+	return reflect.ValueOf(value), errors.New("未知的类型：" + ntype)
+}
 // copy a to b, by the same golang field name
 func Wrap(a interface{}, b interface{}) error {
 	m := structs.Map(a)
