f8faca5fed9d0538b1baba65457f17ba6085b787
tx991020 <1059622332@qq.com>
2020-04-28 09:59:57 +0800

    [add] csv

---
+++ csv.go
@@ -0,0 +1,22 @@
+package utils
+
+import (
+	"encoding/csv"
+	"os"
+)
+
+func ReadCsvFile(filePath string) ([][]string, error) {
+	f, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	csvReader := csv.NewReader(f)
+	records, err := csvReader.ReadAll()
+	if err != nil {
+		return nil, err
+	}
+
+	return records, nil
+}
